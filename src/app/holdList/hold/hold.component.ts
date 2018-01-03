ecfff9fa7822326c4bfbf2bef774c140e3117484
ctnswb <xclai.07@gmail.com>
2018-01-02 21:59:00 -0800

    parent child data binding for hold elements

--- src/app/holdList/hold/hold.component.ts
+++ src/app/holdList/hold/hold.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Hold } from '../hold.model';
 
 @Component({
   selector: 'hold',
   templateUrl: './hold.component.html'
 })
 export class holdComponent {
-
+  @Input() hold: Hold;
 }
